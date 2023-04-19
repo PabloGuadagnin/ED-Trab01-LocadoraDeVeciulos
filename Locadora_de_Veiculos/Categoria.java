75824425c911c99726b0ddd56ad0ca682a7d00ef
Wagner Quadros <wagnerquadros.aluno@unipampa.edu.br>
2023-04-19 19:36:12 -0300

    Create Categoria.java

---
+++ Locadora_de_Veiculos/Categoria.java
@@ -0,0 +1,25 @@
+package Locadora_de_Veiculos;
+
+public class Categoria {
+
+    private String nome;
+    private int identificador;
+   
+    public Categoria(String nome, int identificador){
+        this.nome = nome;
+        this.identificador = identificador;
+    }
+
+
+    public String getNome() {return nome;}
+    public int getIdentificador() {return identificador;}
+    
+    public void setNome(String nome) {this.nome = nome;}
+    public void setIdentificador(int identificador) {this.identificador = identificador;
+    }
+
+    
+
+    
+    
+}
